d668cfa41140f576bcc51c980ac68b3677e03f63
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-12 13:28:18 -0700

    Implement resizer initialization

--- eyecenters.go
+++ eyecenters.go
@@ -1,7 +1,37 @@
 package justeyecenters
 
-import "image"
+import (
+	"image"
 
-func GetEyeCenter(img *image.Image) (*image.Point, error) {
+	"github.com/bamiaux/rez"
+)
+
+var resizer *rez.Converter
+
+func GetEyeCenter(img image.Image) (*image.Point, error) {
+	resized := image.NewGray(image.Rect(0, 0, 64, 64))
+	if resizer == nil {
+		err := initResizer(resized, img)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return nil, nil
+}
+
+func initResizer(output, input image.Image) error {
+	cfg, err := rez.PrepareConversion(output, input)
+	if err != nil {
+		return err
+	}
+
+	cvt, err := rez.NewConverter(cfg, rez.NewBilinearFilter())
+	if err != nil {
+		return err
+	}
+
+	resizer = &cvt
+
+	return nil
 }
